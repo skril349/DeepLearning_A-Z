2314490481f8b1909d3c6d4a1353479535a1fcb2
skril349 <tvives.cabaleiro@gmail.com>
2024-08-10 14:29:11 +0200

    CNN predicting cat or dog image

--- RNC/RNC_dog_cat.py
+++ RNC/RNC_dog_cat.py
@@ -47,12 +47,21 @@ classifier.add(Conv2D(32, kernel_size = (3,3), input_shape = (64,64,3), activati
 # pool_size --> finestra de max pooling
 classifier.add(MaxPooling2D(pool_size = (2,2)))
 
+#Repetim per millorar l'accuracy afegint una segona capa de Convulució i max pooling
+classifier.add(Conv2D(32, kernel_size = (3,3), activation = "relu"))
+classifier.add(MaxPooling2D(pool_size = (2,2)))
+
+#Repetim per millorar l'accuracy afegint una segona capa de Convulució i max pooling
+classifier.add(Conv2D(64, kernel_size = (3,3), activation = "relu"))
+classifier.add(MaxPooling2D(pool_size = (2,2)))
+
 
 #PAS 3 - Flattening
 classifier.add(Flatten())
 
 
 #PAS 4 - Full Connection
+classifier.add(Dense(units = 128, activation="relu"))
 classifier.add(Dense(units = 128, activation="relu"))
 classifier.add(Dense(units = 1, activation="sigmoid"))
 
@@ -92,7 +101,7 @@ test_dataset = test_datagen.flow_from_directory(
 classifier.fit(
         training_dataset,
         steps_per_epoch=8000,#numero de imatges de entrenament
-        epochs=5,
+        epochs=25,
         validation_data=test_dataset,
         validation_steps=2000)
 
@@ -100,10 +109,18 @@ classifier.fit(
 
 
 
+#PREDICCIÓ
 
+import numpy as np
+from keras.preprocessing import image
+test_image = image.load_img("dataset/single_prediction/cat_or_dog_2.jpg",target_size=(64,64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis=0)
+result = classifier.predict(test_image)
 
-
-
-
+if result[0][0] == 1:
+    prediction = "dog"
+else:
+    prediction = "cat"
 
 
