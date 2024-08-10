6176907e7fb15ec542574d0ae50c2cae87ca9945
skril349 <tvives.cabaleiro@gmail.com>
2024-08-10 12:24:30 +0200

    adding RNC dataset

---
+++ RNC/RNC_dog_cat.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 10 11:59:05 2024
+Treballarem amb una estructura de carpetes
+-Dataset
+----Single_prediction
+--------cat_or_dog.png
+-------- ...
+----test_set
+--------cats
+------------cats.png
+
+--------dogs
+------------dogs.png
+
+----training_set
+--------cats
+------------cats.png
+
+--------dogs
+------------dogs.png
+
+@author: tvive
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
